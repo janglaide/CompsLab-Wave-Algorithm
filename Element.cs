5ba0df77154d871b05a91d2ed83285415f0dda00
Alisa Proshchenko <ajanglaide@gmail.com>
2019-05-27 19:34:55 +0300

    written

---
+++ Element.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompsLab
+{
+    class Element
+    {
+        private int _node;
+        private List<int> _connections;
+        private int _value;
+        private bool _isGone;
+        private bool _done;
+        public Element(params int[] arr)
+        {
+            _connections = new List<int>();
+            for (var i = 0; i < arr.Length; i++)
+            {
+                if (i == 0)
+                    _node = arr[i];
+                else
+                    _connections.Add(arr[i]);
+            }
+            Value = -1;
+            _isGone = false;
+            _done = false;
+        }
+
+        public int Node { get => _node; set => _node = value; }
+        public List<int> Connections { get => _connections; set => _connections = value; }
+        public int Value { get => _value; set => _value = value; }
+        public bool IsGone { get => _isGone; set => _isGone = value; }
+        public bool Done { get => _done; set => _done = value; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CompsLab
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter filename: ");
+            var filename = Console.ReadLine();
+
+            if (File.Exists(filename))
+            {
+                string[] textInFile = File.ReadAllLines(filename, Encoding.Default);
+                List<Element> incidentList = new List<Element>();
+                int[] splitted = null;
+                for (var i = 0; i < textInFile.Length; i++)
+                {
+                    splitted = SplitText(textInFile[i]);
+                    incidentList.Add(new Element(splitted));
+                }
+
+                Console.WriteLine("Enter start node: ");
+                var Start = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter end node:");
+                var End = int.Parse(Console.ReadLine());
+
+                List<int> wave = new List<int>();
+                wave.Add(Start);
+                SetValues(incidentList, Start, End, wave);
+
+                var finalValue = incidentList[End - 1].Value;
+
+                List<int> path = new List<int>();
+                FindPath(incidentList, path, End, Start, finalValue, End);
+                var s = "";
+                foreach (var i in path)
+                {
+                    s += i.ToString();
+                    s += " ";
+                }
+                Console.WriteLine(" The path found: " + s);
+               
+                File.WriteAllText("output.txt", s);
+                
+
+            }
+            else
+            {
+                Console.WriteLine("The file does not exist");
+            }
+            Console.ReadKey();
+        }
+
+        static void FindPath(List<Element> list, List<int> res, int currentNode, int start, int valueCounter, int end)
+        {
+            while (currentNode != start)
+            {
+                res.Add(currentNode);
+                var connectionsQuantity = list[currentNode - 1].Connections.Count;
+                var forFlag = 0;
+
+                for (var i = 0; i < connectionsQuantity; i++)
+                {
+                    var min = FindMin(list, currentNode);
+                    FindPath(list, res, list[currentNode - 1].Connections[min], start, list[list[currentNode - 1].Connections[min] - 1].Value, end);
+                    //if (list[list[currentNode - 1].Connections[i] - 1].Value < valueCounter && list[list[currentNode - 1].Connections[i] - 1].IsGone == false)
+                    //{
+                    //    FindPath(list, res, list[currentNode - 1].Connections[i], start, list[list[currentNode - 1].Connections[i] - 1].Value, end);
+                    //}
+                    if (list[end - 1].Done == true)
+                    {
+                        break;
+                    }
+                    forFlag++;
+                }
+                if (forFlag == connectionsQuantity)
+                {
+                    list[currentNode - 1].IsGone = true;
+                    break;
+                }
+                if (list[end - 1].Done == true)
+                {
+                    break;
+                }
+            }
+            if (currentNode == start)
+            {
+                res.Add(currentNode);
+                SetFlags(list, res);
+                list[end - 1].Done = true;
+            }
+
+        }
+
+        static void SetFlags(List<Element> list, List<int> res)
+        {
+            for (var i = 0; i < res.Count; i++)
+            {
+                if (i != 0 && i != res.Count - 1)
+                {
+                    list[res[i] - 1].IsGone = true;
+                }
+            }
+        }
+
+        static bool CheckWaves(int connections, List<Element> list, int node)
+        {
+            for (var i = 0; i < connections; i++)
+            {
+                if (list[list[node - 1].Connections[i] - 1].Value == -1)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        static void SetValues(List<Element> list, int currentNode, int end, List<int> waveList)
+        {
+            var counter = 0;
+            while (counter < waveList.Count)
+            {
+                currentNode = waveList[counter];
+                if (currentNode == end)
+                {
+                    var flag = CheckWaves(list[currentNode - 1].Connections.Count, list, currentNode);
+                    SetMaxValue(list[currentNode - 1].Connections.Count, list, currentNode);
+                    if (flag == false)
+                    {
+                        break;
+                    }
+
+                }
+                else
+                {
+                    if (list[currentNode - 1].Value == -1 || IfEndIsClose(list, currentNode, end))
+                    {
+                        SetMaxValue(list[currentNode - 1].Connections.Count, list, currentNode);
+                        for (var i = 0; i < list[currentNode - 1].Connections.Count; i++)
+                        {
+                            if (list[list[currentNode - 1].Connections[i] - 1].Value == -1 || list[currentNode - 1].Connections[i] == end)
+                            {
+                                waveList.Add(list[currentNode - 1].Connections[i]);
+                            }
+                        }
+                    }
+                }
+                counter++;
+            }
+
+        }
+
+        static bool IfEndIsClose(List<Element> list, int currentNode, int end)
+        {
+            for (var i = 0; i < list[currentNode - 1].Connections.Count; i++)
+            {
+                if (list[list[currentNode - 1].Connections[i] - 1].Value == end)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        static void SetMaxValue(int connections, List<Element> list, int currentNode)
+        {
+            for (var i = 0; i < connections; i++)
+            {
+                if (list[list[currentNode - 1].Connections[i] - 1].Value >= list[currentNode - 1].Value)
+                {
+                    list[currentNode - 1].Value = list[list[currentNode - 1].Connections[i] - 1].Value + 1;
+                }
+            }
+        }
+
+        static int FindMin(List<Element> list, int currentNode)
+        {
+            var val = list[currentNode - 1].Value;
+            var res = -1;
+            for(var i = 0; i < list[currentNode - 1].Connections.Count; i++)
+            {
+                if(list[list[currentNode - 1].Connections[i] - 1].Value < val && list[list[currentNode - 1].Connections[i] - 1].IsGone == false)
+                {
+                    res = i;
+                    val = list[list[currentNode - 1].Connections[i] - 1].Value;
+                }
+            }
+            return res;
+        }
+
+        static int[] SplitText(string text)
+        {
+            List<int> numbers = new List<int>();
+            var flag = false;
+            var ch = "";
+            for (var i = 0; i < text.Length; i++)
+            {
+                if (text[i] != ' ' && flag == false) //не пробел и предыдущий символ -- пробел
+                {
+                    if (i + 1 != text.Length)       //не последний символ
+                    {
+                        if (text[i + 1] != ' ')     //следующий символ -- не пробел
+                        {
+                            ch = text[i].ToString();//запомнить данный символ
+                            flag = true;
+                        }
+                        else
+                        {
+                            numbers.Add(int.Parse(text[i].ToString()));
+                        }
+                    }
+                    else //последний символ -- значит точно однозначное число
+                    {
+                        numbers.Add(int.Parse(text[i].ToString()));
+                    }
+
+                }
+                else if (text[i] != ' ' && flag == true)// не пробел и предыдущий сивол -- не пробел
+                {
+                    var s = ch + text[i];
+                    numbers.Add(int.Parse(s));
+                    flag = false;
+                }
+
+            }
+            int[] result = new int[numbers.Count];
+            for (var i = 0; i < numbers.Count; i++)
+            {
+                result[i] = numbers[i];
+            }
+            return result;
+        }
+    }
+}
